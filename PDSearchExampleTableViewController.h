b1e238e9c4ae169ddf27194b2108ebb41a431824
PAULIUS DRAGUNAS <p@PAULIUSs-MacBook-Air.local>
2015-01-23 18:52:53 -0800

    initial commit

---
+++ PDSearchExampleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDSearchExampleTableViewController.h
+//  UISearchBarInUINavBar
+//
+//  Created by PAULIUS DRAGUNAS on 1/23/15.
+//  Copyright (c) 2015 PauliusDragunas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDSearchExampleTableViewController : UITableViewController
+
+@end
