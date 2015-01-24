b1e0c1410d08475e10929820129768a35a0f121a
PAULIUS DRAGUNAS <p@PAULIUSs-MacBook-Air.local>
2015-01-23 18:13:10 -0800

    Initial Commit

---
+++ UISearchBarInUINavBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISearchBarInUINavBar
+//
+//  Created by PAULIUS DRAGUNAS on 1/23/15.
+//  Copyright (c) 2015 PauliusDragunas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISearchBarInUINavBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UISearchBarInUINavBar
+//
+//  Created by PAULIUS DRAGUNAS on 1/23/15.
+//  Copyright (c) 2015 PauliusDragunas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
